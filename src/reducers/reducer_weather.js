01be052c534c9cc0af3d5fa654197608e481ac0d
Sillas Camara <sillasmc@gmail.com>
2018-04-30 19:57:54 -0300

    Create Reducer for fetch weather

---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,11 @@
+import FETCH_WEATHER from '../actions/index';
+
+export default function (state = [], action) {
+	// Action is already the data result from the Promise
+	// The ReduxPromise Middleware took care of the Promise
+	switch (action.type) {
+		case FETCH_WEATHER:
+			return [ action.payload.data, ...state ];
+	}
+	return state;
+}
